740704c80ceef11cbcdb140f8cc2fb21f8ee3e4b
Claeb101 <Claeb101@users.noreply.github.com>
2020-09-15 02:42:52 +0000

    deploy: 02967f835ce5fff16e18c79bebec760646f5bd7d

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.31183b2eb7b4a461113496dfbbde51a0.js"
+  "/precache-manifest.62cc38873ea99da8f62040825cc10a08.js"
 );
 
 self.addEventListener('message', (event) => {
